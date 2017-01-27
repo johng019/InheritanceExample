5c21f574a67e3376cba9c1aac26997c5c27d1e50
gary0958 <gljfb@yahoo.com>
2017-01-27 15:08:42 -0500

    Inheritance Example

---
+++ InheritanceExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+
+{
+    public class Parent
+    {
+        public string name;
+    }
+
+    public class Father : Parent
+    {
+        public Father()
+        {
+            name = "Gary";
+        } 
+    }
+
+    public class Mother : Parent
+    {
+        public Mother()
+        {
+            name = "Anna";
+        }
+    }
+    public class Program  
+    {
+        static void Main(string[] args)
+        {
+            Father f = new Father();
+            Mother m = new Mother();
+            Console.WriteLine(f.name);
+            Console.WriteLine(m.name);
+            Console.ReadKey();
+        }
+    }
+}
